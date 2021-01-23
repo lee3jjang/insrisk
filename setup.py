a0fb5eee4dfe53ecf513dadc7defa92e3acd7d68
이상진 <lee3jjang@gmail.com>
2021-01-23 21:41:33 +0900

    read me 추가

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
+from os.path import abspath, join as pjoin, relpath, split
 
 DISTNAME = 'insrisk'
 DESCRIPTION = 'Library for Insurance Risk Management'
 AUTHOR = 'SangJin'
 AUTHOR_EMAIL = 'lee3jjang@gmail.com'
 URL = 'https://github.com/lee3jjang/insrisk'
+SETUP_DIR = split(abspath(__file__))[0]
+with open(pjoin(SETUP_DIR, 'README.rst')) as readme:
+    README = readme.read()
+LONG_DESCRIPTION = README
 
 setup(
     name=DISTNAME,
@@ -12,6 +17,7 @@ setup(
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     url=URL,
     packages=['insrisk'],
     python_requires=">=3.6",
